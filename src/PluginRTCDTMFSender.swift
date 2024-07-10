1eeefff10a330eb36ea229ac059c852b88fe67e1
Luigi Previdente <lprevidente@gmail.com>
2024-07-10 15:30:29 +0200

    Check if new rtcRtpSender can be used one Unified-Plan merged

--- src/PluginRTCDTMFSender.swift
+++ src/PluginRTCDTMFSender.swift
@@ -18,10 +18,13 @@ class PluginRTCDTMFSender : NSObject {
 
 		self.eventListener = eventListener
 
-		// TODO check if new rtcRtpSender can be used one Unified-Plan merged
-		//let streamIds = [streamId]
-		//self.rtcRtpSender = rtcPeerConnection.add(track, streamIds: streamIds);
-		self.rtcRtpSender = rtcPeerConnection.senders[0]
+        if rtcPeerConnection.senders.isEmpty {
+            NSLog("PluginRTCDTMFSender#init() rtcPeerConnection.senders isEmpty")
+            let streamIds = [streamId]
+            self.rtcRtpSender = rtcPeerConnection.add(track, streamIds: streamIds);
+        } else {
+            self.rtcRtpSender = rtcPeerConnection.senders[0]
+        }
 
 		if self.rtcRtpSender == nil {
 			NSLog("PluginRTCDTMFSender#init() | rtcPeerConnection.createDTMFSenderForTrack() failed")
